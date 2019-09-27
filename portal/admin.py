6bcc1e245f8e3246c7231e19c875bdcc862f926b
jiviteshjain <jiviteshjain@outlook.com>
2019-09-28 05:27:28 +0530

    Inline admin working

--- portal/admin.py
+++ portal/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from .models import Team, Participant
 
-admin.site.register(Team)
+class TeamInline(admin.StackedInline):
+    model = Team
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (TeamInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Participant)
